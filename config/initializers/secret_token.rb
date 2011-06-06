9089086478e63532f9f9d6bc80914d51fa648ff3
Patrick <patrick.lighthall@gmail.com>
2011-06-06 14:21:37 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FictionGamers::Application.config.secret_token = '59453eaf77a492de91000f823364a9491c78348bc98b39b67efc1c5cdb5d1f900bd533d1d662ff86a12a2faa42eaf8b7f5999e8bef3db8978f982147e2b48583'
